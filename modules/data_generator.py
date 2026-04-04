ea6accb610d2995760dde931df713ce9d5401245
Suryanshu Singh <thesuryanshu16@gmail.com>
2026-04-04 16:48:21 +0530

    Update data_generator.py

--- modules/data_generator.py
+++ modules/data_generator.py
@@ -1,1 +1,7 @@
+import pandas as pd
+import numpy as np
 
+def generate_data(rows, cols):
+    data = np.random.rand(rows, cols)
+    df = pd.DataFrame(data, columns=[f"Feature_{i}" for i in range(cols)])
+    return df
