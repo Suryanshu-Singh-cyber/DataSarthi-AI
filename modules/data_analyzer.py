b61feb1636ba9b80566674cd3a5ddce8c766ed16
Suryanshu Singh <thesuryanshu16@gmail.com>
2026-04-04 23:50:49 +0530

    Update data_analyzer.py

--- modules/data_analyzer.py
+++ modules/data_analyzer.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+def plot_correlation(df):
+    plt.figure(figsize=(6,4))
+    sns.heatmap(df.corr(), annot=False, cmap="coolwarm")
+    return plt
 def analyze_data(df):
     report = {}
     
