dd9173baabf87e9d9b9d05caec9ef05e72135202
Suryanshu Singh <thesuryanshu16@gmail.com>
2026-04-04 16:51:34 +0530

    Update system_checker.py

--- modules/system_checker.py
+++ modules/system_checker.py
@@ -1,1 +1,8 @@
+def check_system(rows, cols, ram_gb):
+    size_bytes = rows * cols * 8
+    size_mb = size_bytes / (1024**2)
 
+    if size_mb > ram_gb * 1024 * 0.5:
+        return "⚠️ Dataset too large for your system"
+    else:
+        return "✅ Dataset can run smoothly"
