50a1a8f0bdce5a75c7b91558a2bb3d306d080890
Suryanshu Singh <thesuryanshu16@gmail.com>
2026-04-04 16:49:46 +0530

    Update data_analyzer.py

--- modules/data_analyzer.py
+++ modules/data_analyzer.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+def analyze_data(df):
+    report = {}
+    
+    report['shape'] = df.shape
+    report['missing'] = df.isnull().sum().to_dict()
+    report['describe'] = df.describe().to_dict()
+    
+    return report
