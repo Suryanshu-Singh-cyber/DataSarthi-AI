668f6702c390c7b41fcd5a8857ba6bbbdeb1e6c1
Suryanshu Singh <thesuryanshu16@gmail.com>
2026-04-04 23:51:54 +0530

    Update data_analyzer.py

--- modules/data_analyzer.py
+++ modules/data_analyzer.py
@@ -2,6 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def data_quality_score(df):
+    missing = df.isnull().sum().sum()
+    total = df.size
+    
+    missing_ratio = missing / total
+    
+    if missing_ratio < 0.05:
+        return "High Quality ✅"
+    elif missing_ratio < 0.2:
+        return "Medium Quality ⚠️"
+    else:
+        return "Low Quality ❌"
+
 def plot_correlation(df):
     plt.figure(figsize=(6,4))
     sns.heatmap(df.corr(), annot=False, cmap="coolwarm")
