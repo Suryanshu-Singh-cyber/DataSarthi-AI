42da69473d247d48e926963ffc16fc50920cb2dc
Suryanshu Singh <thesuryanshu16@gmail.com>
2026-04-04 16:50:33 +0530

    Update model_trainer.py

--- modules/model_trainer.py
+++ modules/model_trainer.py
@@ -1,1 +1,26 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
+def train_models(df):
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    models = {
+        "Logistic Regression": LogisticRegression(),
+        "Decision Tree": DecisionTreeClassifier(),
+        "Random Forest": RandomForestClassifier()
+    }
+
+    results = {}
+
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+        preds = model.predict(X_test)
+        results[name] = accuracy_score(y_test, preds)
+
+    return results
