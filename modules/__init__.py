57efb6cdcfecce6a0717afc93e73e629bf502286
Suryanshu Singh <thesuryanshu16@gmail.com>
2026-05-04 12:49:20 +0530

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,10 +1,25 @@
 # DataSarthi AI - Modules Package
 # This file makes the modules directory a Python package
 
-from .data_generator import SyntheticDataGenerator, generate_data
-from .system_checker import SystemCompatibilityChecker, check_system
-from .model_trainer import ModelTrainer, train_models, apply_pca
-from .recommender import ModelRecommender, recommend_model
+try:
+    from .data_generator import SyntheticDataGenerator, generate_data
+except ImportError:
+    pass
+
+try:
+    from .system_checker import SystemCompatibilityChecker, check_system
+except ImportError:
+    pass
+
+try:
+    from .model_trainer import ModelTrainer, train_models, apply_pca
+except ImportError:
+    pass
+
+try:
+    from .recommender import ModelRecommender, recommend_model
+except ImportError:
+    pass
 
 __all__ = [
     'SyntheticDataGenerator',
